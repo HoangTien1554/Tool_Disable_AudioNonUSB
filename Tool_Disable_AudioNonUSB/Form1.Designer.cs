ef6e4dd13d94703b5bd060ac0b0a3711b7cd4a5e
HoangTien <88817335+hoangtien04@users.noreply.github.com>
2025-03-20 17:08:06 +0700

    Add project files.

---
+++ Tool_Disable_AudioNonUSB/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Tool_Disable_AudioNonUSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Apply = new System.Windows.Forms.Button();
+            this.chk_Audio = new System.Windows.Forms.CheckBox();
+            this.chk_MaxVolume = new System.Windows.Forms.CheckBox();
+            this.lab_SecureBoot = new System.Windows.Forms.Label();
+            this.lab_TPM = new System.Windows.Forms.Label();
+            this.lab_HVCI = new System.Windows.Forms.Label();
+            this.btn_HDSecureboot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Apply
+            // 
+            this.btn_Apply.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.82178F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Apply.Location = new System.Drawing.Point(137, 358);
+            this.btn_Apply.Name = "btn_Apply";
+            this.btn_Apply.Size = new System.Drawing.Size(187, 50);
+            this.btn_Apply.TabIndex = 1;
+            this.btn_Apply.Text = "Apply";
+            this.btn_Apply.UseVisualStyleBackColor = true;
+            this.btn_Apply.Click += new System.EventHandler(this.btn_Apply_Click);
+            // 
+            // chk_Audio
+            // 
+            this.chk_Audio.AutoSize = true;
+            this.chk_Audio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25743F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Audio.Location = new System.Drawing.Point(88, 63);
+            this.chk_Audio.Name = "chk_Audio";
+            this.chk_Audio.Size = new System.Drawing.Size(164, 29);
+            this.chk_Audio.TabIndex = 2;
+            this.chk_Audio.Text = "Disable Audio";
+            this.chk_Audio.UseVisualStyleBackColor = true;
+            this.chk_Audio.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // chk_MaxVolume
+            // 
+            this.chk_MaxVolume.AutoSize = true;
+            this.chk_MaxVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25743F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_MaxVolume.Location = new System.Drawing.Point(88, 101);
+            this.chk_MaxVolume.Name = "chk_MaxVolume";
+            this.chk_MaxVolume.Size = new System.Drawing.Size(150, 29);
+            this.chk_MaxVolume.TabIndex = 2;
+            this.chk_MaxVolume.Text = "Max Volume";
+            this.chk_MaxVolume.UseVisualStyleBackColor = true;
+            // 
+            // lab_SecureBoot
+            // 
+            this.lab_SecureBoot.AutoSize = true;
+            this.lab_SecureBoot.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25743F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_SecureBoot.Location = new System.Drawing.Point(88, 142);
+            this.lab_SecureBoot.Name = "lab_SecureBoot";
+            this.lab_SecureBoot.Size = new System.Drawing.Size(136, 25);
+            this.lab_SecureBoot.TabIndex = 3;
+            this.lab_SecureBoot.Text = "SecureBoot: ";
+            this.lab_SecureBoot.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lab_TPM
+            // 
+            this.lab_TPM.AutoSize = true;
+            this.lab_TPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25743F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_TPM.Location = new System.Drawing.Point(88, 180);
+            this.lab_TPM.Name = "lab_TPM";
+            this.lab_TPM.Size = new System.Drawing.Size(69, 25);
+            this.lab_TPM.TabIndex = 3;
+            this.lab_TPM.Text = "TPM: ";
+            this.lab_TPM.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lab_HVCI
+            // 
+            this.lab_HVCI.AutoSize = true;
+            this.lab_HVCI.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25743F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_HVCI.Location = new System.Drawing.Point(88, 222);
+            this.lab_HVCI.Name = "lab_HVCI";
+            this.lab_HVCI.Size = new System.Drawing.Size(73, 25);
+            this.lab_HVCI.TabIndex = 3;
+            this.lab_HVCI.Text = "HVCI: ";
+            this.lab_HVCI.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_HDSecureboot
+            // 
+            this.btn_HDSecureboot.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.69307F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_HDSecureboot.Location = new System.Drawing.Point(305, 149);
+            this.btn_HDSecureboot.Name = "btn_HDSecureboot";
+            this.btn_HDSecureboot.Size = new System.Drawing.Size(115, 90);
+            this.btn_HDSecureboot.TabIndex = 4;
+            this.btn_HDSecureboot.Text = "Hướng dẫn bật, tắt";
+            this.btn_HDSecureboot.UseVisualStyleBackColor = true;
+            this.btn_HDSecureboot.Click += new System.EventHandler(this.btn_HDSecureboot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(453, 450);
+            this.Controls.Add(this.btn_HDSecureboot);
+            this.Controls.Add(this.lab_HVCI);
+            this.Controls.Add(this.lab_TPM);
+            this.Controls.Add(this.lab_SecureBoot);
+            this.Controls.Add(this.chk_MaxVolume);
+            this.Controls.Add(this.chk_Audio);
+            this.Controls.Add(this.btn_Apply);
+            this.Name = "Form1";
+            this.Text = "lab";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Apply;
+        private System.Windows.Forms.CheckBox chk_Audio;
+        private System.Windows.Forms.CheckBox chk_MaxVolume;
+        private System.Windows.Forms.Label lab_SecureBoot;
+        private System.Windows.Forms.Label lab_TPM;
+        private System.Windows.Forms.Label lab_HVCI;
+        private System.Windows.Forms.Button btn_HDSecureboot;
+    }
+}
+
---
+++ Tool_Disable_AudioNonUSB/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool_Disable_AudioNonUSB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Disable_Audio()
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "Disable_Audio.exe");
+
+            // Ghi file EXE từ Resources ra thư mục tạm
+            File.WriteAllBytes(tempPath, Properties.Resources.Disable_Audio);
+
+            // Tạo ProcessStartInfo để chạy file
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = tempPath, // Chạy từ file đã trích xuất
+                WindowStyle = ProcessWindowStyle.Hidden, // Ẩn cửa sổ
+                CreateNoWindow = true, // Không tạo cửa sổ console
+                UseShellExecute = false // Không sử dụng trình shell mặc định
+            };
+
+            Process process = new Process { StartInfo = startInfo };
+            process.Start();
+        }
+
+        private void Enable_Audio()
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "Enable_Audio.exe");
+
+            // Ghi file EXE từ Resources ra thư mục tạm
+            File.WriteAllBytes(tempPath, Properties.Resources.Enable_Audio);
+
+            // Tạo ProcessStartInfo để chạy file
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = tempPath, // Chạy từ file đã trích xuất
+                WindowStyle = ProcessWindowStyle.Hidden, // Ẩn cửa sổ
+                CreateNoWindow = true, // Không tạo cửa sổ console
+                UseShellExecute = false // Không sử dụng trình shell mặc định
+            };
+
+            Process process = new Process { StartInfo = startInfo };
+            process.Start();
+        }
+        private void Max_Volume()
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "Max_Volume.exe");
+
+            // Ghi file EXE từ Resources ra thư mục tạm
+            File.WriteAllBytes(tempPath, Properties.Resources.Max_Volume);
+
+            // Tạo ProcessStartInfo để chạy file
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = tempPath, // Chạy từ file đã trích xuất
+                WindowStyle = ProcessWindowStyle.Hidden, // Ẩn cửa sổ
+                CreateNoWindow = true, // Không tạo cửa sổ console
+                UseShellExecute = false // Không sử dụng trình shell mặc định
+            };
+
+            Process process = new Process { StartInfo = startInfo };
+            process.Start();
+        }
+
+        private bool getSecureBoot_State()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "powershell.exe";
+                process.StartInfo.Arguments = "-Command \"Confirm-SecureBootUEFI\"";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd().Trim();
+                process.WaitForExit();
+
+                return output == "True"; // Secure Boot được bật nếu trả về "True"
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return false;
+        }
+
+        private bool getTPM_State()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "powershell.exe";
+                process.StartInfo.Arguments = "-Command \"(Get-Tpm).TpmPresent\"";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd().Trim();
+                process.WaitForExit();
+
+                return output == "True"; // TPM được bật nếu trả về "True"
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return false; // Nếu có lỗi, giả định TPM không bật
+        }
+
+        private bool getHVCI_State()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "powershell.exe";
+                process.StartInfo.Arguments = "-Command \"(Get-CimInstance -ClassName Win32_DeviceGuard -Namespace root\\Microsoft\\Windows\\DeviceGuard).AvailableSecurityProperties -contains 3\"";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd().Trim();
+                process.WaitForExit();
+
+                return output == "True"; // HVCI được bật nếu trả về "True"
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return false; // Nếu có lỗi, giả định HVCI không bật
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lab_SecureBoot.Text += getSecureBoot_State().ToString();
+            lab_TPM.Text += getTPM_State().ToString();
+            lab_HVCI.Text += getHVCI_State().ToString();
+                
+        }
+
+        private void openHuongDan()
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "Guide.txt");
+
+            // Lưu file nếu chưa tồn tại
+            if (!File.Exists(tempPath))
+            {
+                File.WriteAllText(tempPath, Properties.Resources.Guide);
+            }
+
+            // Mở file bằng chương trình mặc định
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = tempPath,
+                UseShellExecute = true
+            });
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Apply_Click(object sender, EventArgs e)
+        {
+            if (chk_Audio.Checked == true)
+            {
+                Disable_Audio();
+            }
+            else
+            {
+                Enable_Audio();
+            }
+
+            if(chk_MaxVolume.Checked == true)
+            {
+                Max_Volume();
+            }
+        }
+
+        private void btn_HDSecureboot_Click(object sender, EventArgs e)
+        {
+            openHuongDan();
+        }
+    }
+}
